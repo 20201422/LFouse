c4b7d96f5a9eb2aae729f4cc0ac1b3307212f093
LRX <1531373946@qq.com>
2022-06-21 16:15:41 +0800

    加一个设施类

---
+++ main/src/Model/H_facilities.java
@@ -0,0 +1,67 @@
+package Model;
+
+public class H_facilities {
+    private int h_id;
+    private int wifi;
+    private int TV;
+    private int shower;
+    private int airConditioner;
+    private int washing;
+    private int refrigerator;
+
+    public int getH_id() {
+        return h_id;
+    }
+
+    public void setH_id(int h_id) {
+        this.h_id = h_id;
+    }
+
+    public int getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(int wifi) {
+        this.wifi = wifi;
+    }
+
+    public int getTV() {
+        return TV;
+    }
+
+    public void setTV(int TV) {
+        this.TV = TV;
+    }
+
+    public int getShower() {
+        return shower;
+    }
+
+    public void setShower(int shower) {
+        this.shower = shower;
+    }
+
+    public int getAirConditioner() {
+        return airConditioner;
+    }
+
+    public void setAirConditioner(int airConditioner) {
+        this.airConditioner = airConditioner;
+    }
+
+    public int getWashing() {
+        return washing;
+    }
+
+    public void setWashing(int washing) {
+        this.washing = washing;
+    }
+
+    public int getRefrigerator() {
+        return refrigerator;
+    }
+
+    public void setRefrigerator(int refrigerator) {
+        this.refrigerator = refrigerator;
+    }
+}
