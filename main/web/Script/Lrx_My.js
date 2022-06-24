11de15e65344a0225d25446b15f2f4ef8bc7c3a0
LRX <1531373946@qq.com>
2022-06-24 09:52:12 +0800

    增加展开功能

--- main/web/Script/Lrx_My.js
+++ main/web/Script/Lrx_My.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
-    $("#My").click(function(){
-        if(document.getElementById("MyCz")&&document.getElementById("MyZf")){
-            $("#MyCz").remove();
-            $("#MyZf").remove();
-        }
-        else
-            $("#MyCzOrZf").append("<a href='' id='MyCz'>我的出租&nbsp;</a><a href='' id='MyZf'>&nbsp;我的租房</a>")
-    });
-    $("#My").mouseover(function (){
-        if(!document.getElementById("MyCz")&&!document.getElementById("MyZf"))
-            $("#MyCzOrZf").append("<a href='Lrx_ShowServlet' id='MyCz'>我的出租&nbsp;</a><a href='' id='MyZf'>&nbsp;我的租房</a>")
-    });
-    // $("#My").mouseout(function (){
-    //     if()
+    //$("#My").click(function(){
+       // $("#MyCzOrZf").slideToggle("slow");
+        // if(document.getElementById("MyCz")&&document.getElementById("MyZf")){
+        //     $("#MyCz").remove();
+        //     $("#MyZf").remove();
+        // }
+        // else
+        //     $("#MyCzOrZf").append("<a href='' id='MyCz'>我的出租&nbsp;</a><a href='' id='MyZf'>&nbsp;我的租房</a>")
+    //});
+    // $("#My").mouseover(function (){
+    //     if(!document.getElementById("MyCz")&&!document.getElementById("MyZf"))
+    //         $("#MyCzOrZf").append("<a href='Lrx_ShowServlet' id='MyCz'>我的出租&nbsp;</a><a href='' id='MyZf'>&nbsp;我的租房</a>")
     // });
+    $("#My").mouseenter(function (){
+        $("#MyCzOrZf").slideToggle("slow");
+    });
+    $("#MyCzOrZf").hover(function (){
+        $("#MyCzOrZf").show();
+    },function (){
+        $("#MyCzOrZf").slideToggle("slow");
+    });
+    // $("#My").mouseleave(function (){
+    //     $("#MyCzOrZf").slideToggle("slow");
+    // });
+
 });
